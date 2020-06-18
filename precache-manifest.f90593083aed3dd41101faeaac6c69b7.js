f558aeb0ac9cd8e977120bffae447c51b4d0bff6
Nischal <nischal.gautam08@yahoo.com>
2020-06-18 18:17:28 -0500

    Updates

---
+++ precache-manifest.f90593083aed3dd41101faeaac6c69b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d641b907cacc9984d642f53fb58342d2",
+    "url": "/MemeGenerator/index.html"
+  },
+  {
+    "revision": "9975078e64f2cba35486",
+    "url": "/MemeGenerator/static/css/main.621d71fd.chunk.css"
+  },
+  {
+    "revision": "97652af3cbadbb8de8a5",
+    "url": "/MemeGenerator/static/js/2.b90fae72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MemeGenerator/static/js/2.b90fae72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9975078e64f2cba35486",
+    "url": "/MemeGenerator/static/js/main.d9059837.chunk.js"
+  },
+  {
+    "revision": "c8ea951f6f2b5e0171bb",
+    "url": "/MemeGenerator/static/js/runtime-main.e37821d5.js"
+  }
+]);
